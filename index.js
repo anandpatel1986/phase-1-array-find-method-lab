d04ed37fb55aa04453a61507eed6baa7321f6b71
Anandkumar Patel <avpatel2005@gmail.com>
2023-03-29 17:41:55 -0400

    completes lab

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // code your solution here
+function superbowlWin(record){
+    const foundYear = record.find(element=>element.result === "W");
+    if(foundYear!== undefined)
+        return foundYear.year;
+    else return undefined;
+}
